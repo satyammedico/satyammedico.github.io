abfe18b980fcbd61f9a68c20fe2b9a05072b7520
Pratik K. Yadav <pratikkry@gmail.com>
2019-02-25 19:58:31 +0530

    Update particles.app.js

--- js/particles.app.js
+++ js/particles.app.js
@@ -16,7 +16,7 @@ particlesJS('home',
   {
     "particles": {
       "number": {
-        "value": 80,
+        "value": 120,
         "density": {
           "enable": true,
           "value_area": 800
